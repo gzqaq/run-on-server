44ece8323d10269ba33103449c26ae3fa9444a92
Ziqin Gong <gongzq0301@gmail.com>
2024-04-08 13:57:08 +0800

    execute command on server

--- run_on_server/run.py
+++ run_on_server/run.py
@@ -1,12 +1,8 @@
 import paramiko as ssh
 
 
-def run_on_server(client: ssh.SSHClient, conda_prefix: str, py_file: str,
-                  args: list[str]) -> str:
-  py_exec = f"{conda_prefix}/bin/python3"
-  comm = " ".join([py_exec, py_file] + args)
+def exec_comm(client: ssh.SSHClient, comm: str) -> str:
   _, stdout, stderr = client.exec_command(comm)
-
   outp = stdout.read()
   err_msg = stderr.read()
 
@@ -14,3 +10,11 @@ def run_on_server(client: ssh.SSHClient, conda_prefix: str, py_file: str,
     raise ChildProcessError(err_msg.decode())
 
   return outp.decode()
+
+
+def exec_script(client: ssh.SSHClient, conda_prefix: str, py_file: str,
+                args: list[str]) -> str:
+  py_exec = f"{conda_prefix}/bin/python3"
+  comm = " ".join([py_exec, py_file] + args)
+
+  return exec_comm(client, comm)
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,11 +1,20 @@
 import paramiko as ssh
 
-from run_on_server.run import run_on_server
+from run_on_server.run import exec_comm, exec_script
 from tests.test_client import example_client
 
 
-def test_run_on_server(example_client: ssh.SSHClient) -> None:
-  output = run_on_server(
+def test_exec_comm(example_client: ssh.SSHClient) -> None:
+  output = exec_comm(
+      client=example_client,
+      comm="echo hello",
+  )
+
+  assert output.strip() == "hello"
+
+
+def test_exec_script(example_client: ssh.SSHClient) -> None:
+  output = exec_script(
       client=example_client,
       conda_prefix="/home/zqgong/miniforge3",
       py_file="/home/zqgong/test-script.py",
